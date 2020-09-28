0be8384c4a92a5da0438c8dfcde5c80a4e2c1041
thatsmellything <jensen.judkins@aggiemail.usu.edu>
2020-09-28 16:01:50 -0600

    Made this challenge actually work

---
+++ UltimateChallenge/script.js
@@ -0,0 +1,7 @@
+
+        
+    function printme() {
+        hi = "Hahaha I'm sure you thought this challenge was busted but alas mwahahahaha it is nottttt. I'm curious to how long it took for you to find this little section. I use Firefox and the command line and I know chrome has something like it. Guess I better test that... But anyways your next site is hackermansallix.html GOOOOD LUCKKKKK"
+        console.log(hi);
+        }
+    printme()
